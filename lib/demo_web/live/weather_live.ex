94b3d5b3d876ef91774098eb7821e5fbdb46712a
andrei <andrei@andreirus.com>
2020-04-07 19:23:13 +0200

    conditional nested liveviews

--- lib/demo_web/live/weather_live.ex
+++ lib/demo_web/live/weather_live.ex
@@ -1,3 +1,38 @@
+defmodule DemoWeb.WeatherLive.GrandChild do
+  use Phoenix.LiveView
+
+  def render(assigns) do
+    ~L"""
+    <hr>
+    <p>Grandchild Liveview</p>
+    """
+  end
+end
+
+defmodule DemoWeb.WeatherLive.Child do
+  use Phoenix.LiveView
+
+  def mount(_, _, socket) do
+    {:ok, assign(socket, display: false)}
+  end
+
+  def render(assigns) do
+    ~L"""
+    <p>Child Liveview</p>
+    <a href="#" phx-click="toggle_liveview">Toggle Grandchild Liveview</a>
+    <p>display: <%= @display %></p>
+    <%= if @display do %>
+    <%= live_render(@socket, DemoWeb.WeatherLive.GrandChild, id: :grandchild) %>
+    <% end %>
+    """
+  end
+
+  def handle_event("toggle_liveview", _, socket) do
+    IO.puts("toggle_liveview")
+    {:noreply, update(socket, :display, fn state -> !state end)}
+  end
+end
+
 defmodule DemoWeb.WeatherLive do
   use Phoenix.LiveView
 
@@ -9,16 +44,26 @@ defmodule DemoWeb.WeatherLive do
         <%= @weather %>
       </form>
     </div>
+    <hr>
+    <a href="#" phx-click="toggle_liveview">Toggle Child Liveview</a>
+    <p>display: <%= @display %></p>
+    <%= if @display do %>
+    <%= live_render(@socket, DemoWeb.WeatherLive.Child, id: :child) %>
+    <% end %>
     """
   end
 
   def mount(_params, _session, socket) do
     send(self(), {:put, "Austin"})
-    {:ok, assign(socket, location: nil, weather: "...")}
+    {:ok, assign(socket, location: nil, weather: "...", display: false)}
   end
 
   def handle_event("set-location", %{"location" => location}, socket) do
     {:noreply, put_location(socket, location)}
+  end
+
+  def handle_event("toggle_liveview", _, socket) do
+    {:noreply, update(socket, :display, fn state -> !state end)}
   end
 
   def handle_info({:put, location}, socket) do
--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule Demo.Mixfile do
       {:phoenix_ecto, "~> 4.0"},
       {:ecto_sql, "~> 3.3"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.13"},
+      # {:phoenix_html, "~> 2.13"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
